a418e0135446a70280fb04c75be198e4f1f1ef7e
Deepak S <105696859+deepak0ero7@users.noreply.github.com>
2023-07-04 14:34:31 +0530

    Create Tables.vb

---
+++ Tasks/Tables.vb
@@ -0,0 +1,5 @@
+Dim input As Integer = 15
+        Dim input2 As Integer = 10
+        For num As Integer = 1 To input2
+            Console.WriteLine(input & "x" & num & "=" & input * num)
+        Next
