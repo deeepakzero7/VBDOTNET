629b60971727011e628f6baa291a962b2e8047f3
Deepak S <105696859+deepak0ero7@users.noreply.github.com>
2023-07-10 12:39:10 +0530

    Update Bank.vb

--- Tasks/Bank.vb
+++ Tasks/Bank.vb
@@ -1,1 +1,76 @@
-ok
+   Class BankAccount
+        Private accountNumber As Integer
+        Private ownerName As String
+        Private balance As Double
+        Private transactionHistory As String()
+        Private transactionCount As Integer
+
+        Public Sub New(accNum As Integer, name As String, initialBalance As Double)
+            accountNumber = accNum
+            ownerName = name
+            balance = initialBalance
+            transactionHistory = New String(3) {}
+            transactionCount = 0
+        End Sub
+
+        Public Sub PrintAccountDetails()
+            Console.WriteLine("Account Number: " & accountNumber)
+            Console.WriteLine("Owner Name: " & ownerName)
+            Console.WriteLine("Balance: $" & balance)
+        End Sub
+
+        Public Sub AddTransaction(transaction As String)
+            If transactionCount < 4 Then
+                transactionHistory(transactionCount) = transaction
+            Else
+                ' Shift transactions to the left to make space for the new transaction
+                For i As Integer = 0 To transactionHistory.Length - 2
+                    transactionHistory(i) = transactionHistory(i + 1)
+                Next
+                transactionHistory(transactionHistory.Length - 1) = transaction
+            End If
+
+            transactionCount += 1
+        End Sub
+
+        Public Sub PrintTransactionHistory()
+            Console.WriteLine("----- Transaction History -----")
+            For Each transaction As String In transactionHistory
+                If transaction IsNot Nothing Then
+                    Console.WriteLine(transaction)
+                End If
+            Next
+        End Sub
+
+        Public Sub Deposit(amount As Double)
+            balance += amount
+            Dim transaction As String = "Deposit: $" & amount
+            AddTransaction(transaction)
+        End Sub
+
+        Public Sub Withdraw(amount As Double)
+            If amount <= balance Then
+                balance -= amount
+                Dim transaction As String = "Withdrawal: $" & amount
+                AddTransaction(transaction)
+            Else
+                Console.WriteLine("Insufficient funds!")
+            End If
+        End Sub
+    End Class
+
+
+Function CreateAccount() As BankAccount
+        Console.WriteLine("----- Bank Account Creation -----")
+        Console.Write("Enter Account Number: ")
+        Dim accNum As Integer = Integer.Parse(Console.ReadLine())
+
+        Console.Write("Enter Owner Name: ")
+        Dim name As String = Console.ReadLine()
+
+        Console.Write("Enter Initial Balance: $")
+        Dim initialBalance As Double = Double.Parse(Console.ReadLine())
+
+        Return New BankAccount(accNum, name, initialBalance)
+    End Function
+End Module
